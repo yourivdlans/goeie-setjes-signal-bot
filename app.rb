0e5f06b706afea6af45245c38c6a5e575561ef22
Youri van der Lans <youri@itflows.nl>
2020-01-29 11:42:39 +0100

    Use open3 to execute command and read output line by line.

--- app.rb
+++ app.rb
@@ -1,19 +1,24 @@
 require 'sinatra'
+require 'open3'
 
 class GoeieSetjesSignalBotApp < Sinatra::Base
   get '/messages.json' do
     content_type :json
 
-    messages = %x[signal-cli -u #{ENV.fetch('SIGNAL_USER_ACCOUNT')} receive --json]
+    cmd = "signal-cli -u #{ENV.fetch('SIGNAL_USER_ACCOUNT')} receive --json"
 
-    exposed_messages = messages.split("\n").map do |message|
-      json_message = JSON.parse(message)
+    exposed_messages = []
 
-      group_id = json_message.dig('envelope', 'dataMessage', 'groupInfo', 'groupId') || json_message.dig('envelope', 'syncMessage', 'sentMessage', 'groupInfo', 'groupId')
-      next if group_id.nil? || group_id == '' || group_id != ENV.fetch('SIGNAL_GROUP_ID')
+    Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
+      while line = stdout.gets
+        json_message = JSON.parse(line)
 
-      json_message.to_json
-    end.compact
+        group_id = json_message.dig('envelope', 'dataMessage', 'groupInfo', 'groupId') || json_message.dig('envelope', 'syncMessage', 'sentMessage', 'groupInfo', 'groupId')
+        next if group_id.nil? || group_id == '' || group_id != ENV.fetch('SIGNAL_GROUP_ID')
+
+        exposed_messages << json_message.to_json
+      end
+    end
 
     return {}.to_json if exposed_messages.length == 0
 
