076946a8f6576f7f360f6a66d1f627cdb188087f
Youri van der Lans <youri@itflows.nl>
2021-09-24 19:50:03 +0200

    Always trust new identities.

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ dbus_address=`dbus-daemon --config-file=/usr/share/dbus-1/session.conf --print-a
 
 export DBUS_SESSION_BUS_ADDRESS=$dbus_address
 
-signal-cli -u $SIGNAL_USER_ACCOUNT daemon &
+signal-cli -u $SIGNAL_USER_ACCOUNT --trust-new-identities=always daemon &
 
 echo "Starting signal bot with $SIGNAL_USER_ACCOUNT..."
 
