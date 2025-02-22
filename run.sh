7e9886568ecf4b6c859ca7fe10f30e1a968543e6
Youri van der Lans <youri@itflows.nl>
2025-02-22 20:21:46 +0100

    Running signal-cli without the —dbus argument is deprecated

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/tmp/ruby-dbus"
 DBUS_SESSION_BUS_PID=$(dbus-daemon --config-file=/usr/share/dbus-1/session.conf --fork --print-pid --address="$DBUS_SESSION_BUS_ADDRESS")
 
-signal-cli -u $SIGNAL_USER_ACCOUNT --trust-new-identities=always daemon &
+signal-cli -u $SIGNAL_USER_ACCOUNT --trust-new-identities=always daemon --dbus &
 
 echo "Starting signal bot with $SIGNAL_USER_ACCOUNT..."
 
